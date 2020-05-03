5bb3d4d383ca95bedbec228db0556ba685bcc4b3
Marie-Josee Blais <maelen000@gmail.com>
2020-05-03 11:40:24 -0400

    Updated scripts for tvheadend

--- convert_video.sh
+++ convert_video.sh
@@ -14,7 +14,7 @@ if [ -e "$inputFile" ]; then
 #echo inputFile $inputFile
 #echo outputFile $outputFile
 
-	/usr/bin/ffmpeg -i "$inputFile" -c:v libx264 -vf "yadif=0:-1:0,scale=min(720\,iw):trunc(ow/a/2)*2" -preset slow -crf 21 -b:a 128k -sn "$outputFile"
+	/usr/bin/ffmpeg -y -i "$inputFile" -c:v libx264 -vf "yadif=0:-1:0,scale=min(720\,iw):trunc(ow/a/2)*2" -preset slow -crf 21 -b:a 128k -sn "$outputFile"
 else
 	echo "File does not exist"
 fi
