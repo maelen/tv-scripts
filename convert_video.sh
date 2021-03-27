e23698b1d18f8c6d8f2528294cbe91b85aa35b88
Marie-Josee Blais <maelen000@gmail.com>
2021-03-27 17:49:44 -0400

    Added tvheadend cron script and tab clean-up

--- convert_video.sh
+++ convert_video.sh
@@ -4,17 +4,17 @@ inputFile="$1"
 outputFile="$2"
 
 if [ -e "$inputFile" ]; then
-	filebase=$(basename "$inputFile")
-	if [ -d "$2" ]; then
-		outputFile="$2"/${filebase%.*}.mkv
-	elif [ "$outputFile" = "" ]; then
-		outputFile=$(dirname "$inputFile")/${filebase%.*}_output.mkv
-	fi
+    filebase=$(basename "$inputFile")
+    if [ -d "$2" ]; then
+        outputFile="$2"/${filebase%.*}.mkv
+    elif [ "$outputFile" = "" ]; then
+        outputFile=$(dirname "$inputFile")/${filebase%.*}_output.mkv
+    fi
 
 #echo inputFile $inputFile
 #echo outputFile $outputFile
 
-	/usr/bin/ffmpeg -y -i "$inputFile" -vf scale=1280:-1 -c:v libx264 -preset slow -crf 24 "$outputFile"
+    /usr/bin/ffmpeg -y -i "$inputFile" -vf scale=1280:-1 -c:v libx264 -preset slow -crf 24 "$outputFile"
 else
-	echo "File does not exist"
+    echo "File does not exist"
 fi
